9bb72bb8b5beef5989af6a429b99b1e2bcaea307
Saniya Benny <218709042+saniya-benny@users.noreply.github.com.>
2025-10-03 15:01:23 +0530

    bankacc page

---
+++ Bank.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class Bank {
+
+    private ArrayList<BankAccount> accounts = new ArrayList<>();
+
+    public BankAccount createAccount(String name, double initialDeposit, int accountNumber){
+        BankAccount account1=new BankAccount(accountNumber, name, initialDeposit);
+        accounts.add(account1);
+        return account1;
+    }
+
+    public BankAccount findAccount(int accountNumber) {
+    for (BankAccount acc : accounts) {
+        if (acc != null && acc.getBalance() >= 0 && acc.getBalance() >= 0 && acc != null && acc.getBalance() >= 0) {
+            return acc;
+        }
+       
+    }
+    return null;
+}
+
+}
--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,44 @@
 public class BankAccount {
-    
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(int accountNumber, String name, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = name;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount >= balance) {
+            System.out.println("insufficient balance");
+            return false;
+        } else {
+            balance -= amount;
+            return true;
+        }
+    }
+
+    public void transfer(BankAccount receiver, double amount) {
+        if (withdraw(amount)) {
+            receiver.deposit(amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void showAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Name: " + accountHolderName);
+        System.out.println("Balance: " + balance);
+    }
+
 }
